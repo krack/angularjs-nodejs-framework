d24a34eaef42b7064a0fbf148d96c7073914a77b
Sylvain Gandon <krack_6@hotmail.com>
2017-04-29 17:27:17 +0200

    add server module

---
+++ index.js
@@ -0,0 +1,6 @@
+
+var configureAPI = require('./rest/rest-api-database');
+var auth = require('./authentication/authentification.module');
+
+export.configureAPI = configureAPI;
+export.auth = auth;
