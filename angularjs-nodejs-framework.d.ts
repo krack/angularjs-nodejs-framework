a74a9839ea5653f24c493120ea5ecfc132eeee39
Sylvain Gandon <krack_6@hotmail.com>
2017-05-02 14:01:34 +0200

    add file model

--- angularjs-nodejs-framework.d.ts
+++ angularjs-nodejs-framework.d.ts
@@ -1,2 +1,3 @@
 export * from './client/components/login/login.component';
 export * from './client/components/files-uploader/files-uploader.component';
+export * from './client/components/files-uploader/file';
--- angularjs-nodejs-framework.js
+++ angularjs-nodejs-framework.js
@@ -1,3 +1,4 @@
 export * from './client/components/login/login.component';
 export * from './client/components/files-uploader/files-uploader.component';
+export * from './client/components/files-uploader/file';
 //# sourceMappingURL=/home/krack/projects/angularjs-nodejs-framework/angularjs-nodejs-framework.js.map
